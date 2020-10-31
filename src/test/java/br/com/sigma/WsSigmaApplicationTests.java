f109018d6515da6760afdbc58bdacc4204974db3
guilherme costa <kzaa22@gmail.com>
2020-10-30 20:48:49 -0400

    create project

---
+++ src/test/java/br/com/sigma/WsSigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
