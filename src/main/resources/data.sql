4546353de6a6ba2769858cc6a229d0e7c17621f1
guilherme costa <kzaa22@gmail.com>
2020-10-31 22:24:55 -0400

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into country values(1,'Brasil');
+insert into state values(1,'Amazonas',1);
+insert into city values(1,'Manaus',1);
+insert into neighbourhood values(1,'Cidade de Deus',1);
+insert into company values(1,'???','ltda','Cidade de Deus','12345678','00000000',1);
