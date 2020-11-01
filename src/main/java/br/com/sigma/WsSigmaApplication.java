7c4a7968075d83a452340f0380a2df2d5f13feaf
guilherme costa <kzaa22@gmail.com>
2020-10-31 20:57:20 -0400

    updates in main and generic service

--- src/main/java/br/com/sigma/WsSigmaApplication.java
+++ src/main/java/br/com/sigma/WsSigmaApplication.java
@@ -2,12 +2,20 @@ package br.com.sigma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class WsSigmaApplication {
+@EnableSwagger2
+public class WsSigmaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsSigmaApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(WsSigmaApplication.class);
+	}
 }
--- src/main/java/br/com/sigma/utils/GenericServiceImpl.java
+++ src/main/java/br/com/sigma/utils/GenericServiceImpl.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 import java.util.Optional;
 
 @Service
-public class GenericServiceImpl <E extends BaseEntity<ID>, ID extends Serializable> implements GenericService<E,ID> {
+public abstract class GenericServiceImpl <E extends BaseEntity<ID>, ID extends Serializable> implements GenericService<E,ID> {
 
     @Autowired
     private JpaRepository<E,ID> repository;
